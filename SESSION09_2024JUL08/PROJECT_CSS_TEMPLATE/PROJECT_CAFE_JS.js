b8cd5f10e996a785922b4dcb6c3d05080e65f274
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-07-12 18:27:13 +0300

    Added a javascript file

---
+++ SESSION09_2024JUL08/PROJECT_CSS_TEMPLATE/PROJECT_CAFE_JS.js
@@ -0,0 +1,19 @@
+// PROJECT_CAFE_JS.js
+
+$(document).ready(function() {
+  var lastScrollTop = 0;
+  var navbar = $('#mainNav');
+
+  $(window).scroll(function(event) {
+      var st = $(this).scrollTop();
+
+      if (st > lastScrollTop){
+          // Downscroll code
+          navbar.addClass('smaller');
+      } else {
+          // Upscroll code
+          navbar.removeClass('smaller');
+      }
+      lastScrollTop = st;
+  });
+});
